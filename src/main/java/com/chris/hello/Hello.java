650720d078202cb584bfb4f21cc3795ac250f348
ChrisLijoodi <lijoodichristopher@gmail.com>
2025-12-25 13:41:13 +0300

    Hello World

---
+++ src/main/java/com/chris/hello/Hello.java
@@ -0,0 +1,17 @@
+package com.chris.hello;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/")
+public class Hello {
+    @GetMapping
+    public ResponseEntity<String> helloWorld(){
+        return new ResponseEntity<>("Hello World!", HttpStatus.OK);
+    }
+}
